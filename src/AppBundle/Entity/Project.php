e51f7b98badc44c80bf128a80f5fcc67c3b4201d
Djamel Ferhod <djamel.ferhod@ish-lyon.cnrs.fr>
2017-07-25 10:47:47 +0200

    Création du fichier de classe Project.

---
+++ src/AppBundle/Entity/Project.php
@@ -0,0 +1,160 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Djamel
+ * Date: 19/07/2017
+ * Time: 16:19
+ */
+
+namespace AppBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * Class Project
+ * @ORM\Entity
+ * @ORM\Table(schema="che", name="project")
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     * @ORM\Column(type="integer", name="pk_label")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $startDate;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $endDate;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $notes;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $standardLabel;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Project")
+     * @ORM\JoinColumn(name="fk_is_subproject_of", referencedColumnName="pk_project", nullable=true)
+     */
+    private $parentProject;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="creator", referencedColumnName="pk_user", nullable=false)
+     */
+    private $creator;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\ManyToOne(targetEntity="User")
+     * @ORM\JoinColumn(name="modifier", referencedColumnName="pk_user", nullable=false)
+     */
+    private $modifier;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $creationTime;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $modificationTime;
+
+    /**
+     * @return mixed
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStartDate()
+    {
+        return $this->startDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getEndDate()
+    {
+        return $this->endDate;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getNotes()
+    {
+        return $this->notes;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStandardLabel()
+    {
+        return $this->standardLabel;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getParentProject()
+    {
+        return $this->parentProject;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreator()
+    {
+        return $this->creator;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModifier()
+    {
+        return $this->modifier;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getCreationTime()
+    {
+        return $this->creationTime;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getModificationTime()
+    {
+        return $this->modificationTime;
+    }
+
+
+
+}
