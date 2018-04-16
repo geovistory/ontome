313b833669a61146d1aae85183446a774e722713
Djamel Ferhod <djamel.ferhod@ish-lyon.cnrs.fr>
2018-04-16 21:43:02 +0200

    Ajout du creator et du modifier en hidden

--- src/AppBundle/Form/TextPropertyType.php
+++ src/AppBundle/Form/TextPropertyType.php
@@ -3,16 +3,40 @@
 namespace AppBundle\Form;
 
 use AppBundle\Entity\TextProperty;
+use AppBundle\Form\DataTransformer\UserToNumberTransformer;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\HiddenType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInterface;
 
 class TextPropertyType extends AbstractType
 {
+    private $transformer;
+    private $tokenStorage;
+    private $em;
+
+    public function __construct(UserToNumberTransformer $transformer, TokenStorageInterface $tokenStorage, EntityManagerInterface $em)
+    {
+        $this->transformer = $transformer;
+        $this->tokenStorage = $tokenStorage;
+        $this->em = $em;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
+        $userID = $this->tokenStorage->getToken()->getUser()->getId();
+        $user = $this->em->getRepository('AppBundle:User')->find($userID);
+
+        if (!$user) {
+            throw new \LogicException(
+                'The TextPropertyType cannot be used without an authenticated user!'
+            );
+        }
+
         $builder
             ->add('textProperty', TextareaType::class, array(
                 'attr' => array('class' => 'tinymce')
@@ -24,8 +48,16 @@ class TextPropertyType extends AbstractType
                     'German' => 'de',
                     'Italian' => 'it',
                     'Spanish' => 'es'
-                )
-            ));
+                ),
+                'label' => 'Language'
+            ))
+            ->add('creator', HiddenType::class)
+            ->add('modifier', HiddenType::class);
+        $builder->get('creator')
+            ->addModelTransformer($this->transformer);
+        $builder->get('modifier')
+            ->addModelTransformer($this->transformer);
+
     }
 
     public function configureOptions(OptionsResolver $resolver)
