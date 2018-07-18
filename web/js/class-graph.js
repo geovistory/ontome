d71863b1d5fdd7d836ba92cea988f9684eb0e22e
Djamel Ferhod <djamel.ferhod@ish-lyon.cnrs.fr>
2018-07-18 15:59:52 +0200

    Généralisation de la fonctionnalité permettant d'ouvrir un onglet spécifique d'une page.

--- web/js/class-graph.js
+++ web/js/class-graph.js
@@ -1,16 +1,3 @@
-$(function() {
-    // Javascript to enable link to tab
-    var hash = document.location.hash;
-    if (hash) {
-        console.log(hash);
-        $('.nav-tabs a[href="'+hash+'"]').tab('show');
-    }
-
-    // Change hash for page-reload
-    $('a[data-toggle="tab"]').on('show.bs.tab', function (e) {
-        window.location.hash = e.target.hash;
-    });
-});
 function reset() {
     location.reload();
 }
