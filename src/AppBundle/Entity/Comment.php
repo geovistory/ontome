e1a227d89bbb80bcea1c8070b8199c41d37f675e
Djamel Ferhod <djamel.ferhod@ish-lyon.cnrs.fr>
2018-10-09 13:42:10 +0200

    Création de la classe (vide) des commentaires

---
+++ src/AppBundle/Entity/Comment.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Djamel
+ * Date: 04/10/2018
+ * Time: 13:40
+ */
+
+namespace AppBundle\Entity;
+
+/**
+ * Class Comment
+ * @ORM\Table(schema="che", name="comment")
+ */
+class Comment
+{
+
+}
