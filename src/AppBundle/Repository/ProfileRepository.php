c156345552d0234a98d11e4bd208ce4150d3d9a1
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2019-06-11 10:55:14 +0200

    Graphe de profil (manque plus que le SQL à corriger)

---
+++ src/AppBundle/Repository/ProfileRepository.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Djamel
+ * Date: 23/06/2017
+ * Time: 14:57
+ */
+
+namespace AppBundle\Repository;
+
+use AppBundle\Entity\Profile;
+use Doctrine\ORM\EntityRepository;
+
+class ProfileRepository extends EntityRepository
+{
+
+    /**
+     * @return array
+     */
+    public function findProfileGraph(Profile $profile){
+        $conn = $this->getEntityManager()
+            ->getConnection();
+
+        $sql = "SELECT get_webvowl_json_for_profile AS json FROM api.get_webvowl_json_for_profile(:profile)";
+
+        $stmt = $conn->prepare($sql);
+        $stmt->execute(array('profile' => $profile->getId()));
+
+        return $stmt->fetchAll();
+    }
+}
