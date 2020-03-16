1c3930ada4553c30883689ff7125472a2251af4a
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2020-03-16 10:37:39 +0100

    905 - Création du canDuplicate (ProfileVoter) et update du code ProfileController

--- src/AppBundle/Security/ProfileVoter.php
+++ src/AppBundle/Security/ProfileVoter.php
@@ -16,11 +16,12 @@ use Symfony\Component\Security\Core\Authorization\Voter\Voter;
 class ProfileVoter extends Voter
 {
     const EDIT = 'edit';
+    const DUPLICATE = 'duplicate';
 
     protected function supports($attribute, $subject)
     {
         // if the attribute isn't one we support, return false
-        if (!in_array($attribute, array(self::EDIT))) {
+        if (!in_array($attribute, array(self::EDIT, self::DUPLICATE))) {
             return false;
         }
 
@@ -47,6 +48,8 @@ class ProfileVoter extends Voter
         switch ($attribute) {
             case self::EDIT:
                 return $this->canEdit($profile, $user);
+            case self::DUPLICATE:
+                return $this->canDuplicate($profile, $user);
         }
 
         throw new \LogicException('This code should not be reached!');
@@ -76,5 +79,28 @@ class ProfileVoter extends Voter
             return true;
         }*/
         return $canEdit;
+    }
+
+    /**
+     * @param Profile $profile
+     * @param User $user
+     * @return bool TRUE if $user is the creator of $profile and the profile is published and not deprecated
+     */
+    private function canDuplicate(Profile $profile, User $user)
+    {
+        $canDuplicate = false;
+
+        foreach ($user->getUserProjectAssociations()->getIterator() as $i => $userProjectAssociation) {
+            if ($userProjectAssociation->getProject()->getOwnedProfiles()->contains($profile) && $userProjectAssociation->getPermission() <= 2 ) { //permission <= means that the user is a project admin or manager
+                $canDuplicate = true;
+            }
+        }
+
+        if($profile->getIsOngoing() and is_null($profile->getWasClosedAt()) and !is_null($profile->getEndDate()))
+        {
+            $canDuplicate = false;
+        }
+
+        return $canDuplicate;
     }
 }
