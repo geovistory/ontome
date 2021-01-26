6a726dc55b5925fa3e089eeb4f39f940ddc68dda
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2021-01-26 17:08:55 +0100

    Oubli 971 au niveau des propriétés

--- src/AppBundle/Controller/PropertyController.php
+++ src/AppBundle/Controller/PropertyController.php
@@ -249,6 +249,26 @@ class PropertyController extends Controller
 
         $em = $this->getDoctrine()->getManager();
 
+        //Si le namespace n'est pas specifié dans l'url mais dans My Current Namespace, rediriger
+        // $namespacesIdFromUser : Ensemble de tous les namespaces activés par l'utilisateur
+
+        if(is_null($namespaceFromUrlId)) {
+            if (is_null($this->getUser()) || $this->getUser()->getCurrentActiveProject()->getId() == 21) {
+                $namespacesIdFromUser = $em->getRepository('AppBundle:OntoNamespace')->findPublicProjectNamespacesId();
+            } else { // Utilisateur connecté et utilisant un autre projet
+                $namespacesIdFromUser = $em->getRepository('AppBundle:OntoNamespace')->findNamespacesIdByUser($this->getUser());
+            }
+            foreach ($namespacesIdFromUser as $namespaceIdFromUser) {
+                $namespaceFromUser = $em->getRepository('AppBundle:OntoNamespace')->find($namespaceIdFromUser);
+                if ($propertyVersion->getNamespaceForVersion()->getTopLevelNamespace()->getId() === $namespaceFromUser->getTopLevelNamespace()->getId()) {
+                    return $this->redirectToRoute('property_show_with_version', [
+                        'id' => $property->getId(),
+                        'namespaceFromUrlId' => $namespaceIdFromUser
+                    ]);
+                }
+            }
+        }
+
         // $namespacesIdFromClassVersion : Ensemble de namespaces provenant de la classe affiché (namespaceForVersion + references)
         $namespacesIdFromPropertyVersion[] = $propertyVersion->getNamespaceForVersion()->getId();
 
