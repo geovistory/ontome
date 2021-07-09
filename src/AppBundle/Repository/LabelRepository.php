791421d2728663fb9de5f89ae7253ec05d297885
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2021-07-09 16:46:45 +0200

    Tri par pertinence Correction bug entités HTML (faire la requête)

--- src/AppBundle/Repository/LabelRepository.php
+++ src/AppBundle/Repository/LabelRepository.php
@@ -29,31 +29,37 @@ class LabelRepository extends EntityRepository
         // On concatene label avec inverse_label pour chercher les 2 en même temps
         // Il y a plusieurs unions car il faut parfois récupérer l'identifier, le standard label de l'entité ou le standard label du NS...
 
-        $sql = "SELECT lbl.*, c.identifier_in_namespace, cv.standard_label, ns.standard_label as ns_standard_label FROM che.label lbl
+        $sql = "SELECT lbl.*, c.identifier_in_namespace, cv.standard_label, ns.standard_label as ns_standard_label, ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score 
+                FROM che.label lbl
                 JOIN che.class c ON lbl.fk_class = c.pk_class 
                 JOIN che.class_version cv ON lbl.fk_class = cv.fk_class AND lbl.fk_namespace_for_version = cv.fk_namespace_for_version
                 JOIN che.namespace ns ON lbl.fk_namespace_for_version = ns.pk_namespace
                 WHERE lbl.fk_class IS NOT NULL AND to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT lbl.*, p.identifier_in_namespace, pv.standard_label, ns.standard_label as ns_standard_label FROM che.label lbl
+                    UNION 
+                SELECT lbl.*, p.identifier_in_namespace, pv.standard_label, ns.standard_label as ns_standard_label, ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score 
+                FROM che.label lbl
                 JOIN che.property p ON lbl.fk_property = p.pk_property
                 JOIN che.property_version pv ON lbl.fk_property = pv.fk_property AND lbl.fk_namespace_for_version = pv.fk_namespace_for_version
                 JOIN che.namespace ns ON lbl.fk_namespace_for_version = ns.pk_namespace
                 WHERE lbl.fk_property IS NOT NULL AND to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT lbl.*, '', ns.standard_label, '' as ns_standard_label FROM che.label lbl
+                    UNION 
+                SELECT lbl.*, '', ns.standard_label, '' as ns_standard_label , ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score
+                FROM che.label lbl
                 JOIN che.namespace ns ON lbl.fk_namespace = ns.pk_namespace
                 WHERE lbl.fk_namespace IS NOT NULL AND to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT lbl.*, '', prf.standard_label, '' as ns_standard_label FROM che.label lbl
+                    UNION 
+                SELECT lbl.*, '', prf.standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score 
+                FROM che.label lbl
                 JOIN che.profile prf ON lbl.fk_profile = prf.pk_profile
                 WHERE lbl.fk_profile IS NOT NULL AND to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT lbl.*, '', prj.standard_label, '' as ns_standard_label FROM che.label lbl
+                    UNION 
+                SELECT lbl.*, '', prj.standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score
+                FROM che.label lbl
                 JOIN che.project prj ON lbl.fk_project = prj.pk_project
                 WHERE lbl.fk_project IS NOT NULL AND to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT lbl.*, '' as identifier_in_namespace, '' as standard_label, '' as ns_standard_label FROM che.label lbl
+                    UNION 
+                SELECT lbl.*, '' as identifier_in_namespace, '' as standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', CONCAT(lbl.label, ' ', lbl.inverse_label)), plainto_tsquery('english', :query)) as score 
+                FROM che.label lbl
                 WHERE lbl.fk_property IS NULL  
                   AND lbl.fk_class IS NULL 
                   AND lbl.fk_namespace IS NULL 
--- src/AppBundle/Repository/TextPropertyRepository.php
+++ src/AppBundle/Repository/TextPropertyRepository.php
@@ -27,31 +27,35 @@ class TextPropertyRepository extends EntityRepository
         // Rechercher tous les txtp correspondant à la recherche
         // comparateur: to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
         // Il y a plusieurs unions car il faut parfois récupérer l'identifier, le standard label de l'entité ou le standard label du NS...
-        $sql = "SELECT txtp.*, c.identifier_in_namespace, cv.standard_label, ns.standard_label as ns_standard_label FROM che.text_property txtp
+        $sql = "SELECT txtp.*, c.identifier_in_namespace, cv.standard_label, ns.standard_label as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score 
+                FROM che.text_property txtp
                 JOIN che.class c ON txtp.fk_class = c.pk_class 
                 JOIN che.class_version cv ON txtp.fk_class = cv.fk_class AND txtp.fk_namespace_for_version = cv.fk_namespace_for_version
                 JOIN che.namespace ns ON txtp.fk_namespace_for_version = ns.pk_namespace
                 WHERE txtp.fk_class IS NOT NULL AND to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT txtp.*, p.identifier_in_namespace, pv.standard_label, ns.standard_label as ns_standard_label FROM che.text_property txtp
+                    UNION 
+                SELECT txtp.*, p.identifier_in_namespace, pv.standard_label, ns.standard_label as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score 
+                FROM che.text_property txtp
                 JOIN che.property p ON txtp.fk_property = p.pk_property
                 JOIN che.property_version pv ON txtp.fk_property = pv.fk_property AND txtp.fk_namespace_for_version = pv.fk_namespace_for_version
                 JOIN che.namespace ns ON txtp.fk_namespace_for_version = ns.pk_namespace
                 WHERE txtp.fk_property IS NOT NULL AND to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT txtp.*, '', ns.standard_label, '' as ns_standard_label FROM che.text_property txtp
+                    UNION 
+                SELECT txtp.*, '', ns.standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score 
+                FROM che.text_property txtp
                 JOIN che.namespace ns ON txtp.fk_namespace = ns.pk_namespace
                 WHERE txtp.fk_namespace IS NOT NULL AND to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT txtp.*, '', prf.standard_label, '' as ns_standard_label FROM che.text_property txtp
+                    UNION 
+                SELECT txtp.*, '', prf.standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score 
+                FROM che.text_property txtp
                 JOIN che.profile prf ON txtp.fk_profile = prf.pk_profile
                 WHERE txtp.fk_profile IS NOT NULL AND to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT txtp.*, '', prj.standard_label, '' as ns_standard_label FROM che.text_property txtp
+                    UNION 
+                SELECT txtp.*, '', prj.standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score FROM che.text_property txtp
                 JOIN che.project prj ON txtp.fk_project = prj.pk_project
                 WHERE txtp.fk_project IS NOT NULL AND to_tsvector('english', txtp.text_property) @@ plainto_tsquery('english', :query)
-                UNION 
-                SELECT txtp.*, '' as identifier_in_namespace, '' as standard_label, '' as ns_standard_label FROM che.text_property txtp
+                    UNION 
+                SELECT txtp.*, '' as identifier_in_namespace, '' as standard_label, '' as ns_standard_label, ts_rank(to_tsvector('english', txtp.text_property), plainto_tsquery('english', :query)) as score FROM che.text_property txtp
                 WHERE txtp.fk_property IS NULL 
                   AND txtp.fk_class IS NULL 
                   AND txtp.fk_namespace IS NULL 
