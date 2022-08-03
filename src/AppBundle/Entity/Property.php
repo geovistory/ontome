44fce8dd0c607eecb35757e8dc94aa2f593c06cd
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2022-08-03 14:41:28 +0200

    Ticket 1020 - Unicité des identifiants dans les namespaces pour tous les entités

--- src/AppBundle/Entity/Property.php
+++ src/AppBundle/Entity/Property.php
@@ -187,15 +187,26 @@ class Property
             // (d'autres namespaces du même root mais qui n'ont pas cette classe, peuvent donc échapper)
             // il faut donc simplement récupérer le root et boucler dessus
             $rootNamespace = $this->getPropertyVersionForDisplay()->getNamespaceForVersion()->getTopLevelNamespace();
+            $uniqueIdentifiant = true;
             foreach ($rootNamespace->getChildVersions() as $namespace) {
                 foreach ($namespace->getProperties() as $property) {
                     if ($property->identifierInNamespace == $this->identifierInNamespace and $property != $this) {
-                        $context->buildViolation('The identifier must be unique. Please enter another one.')
-                            ->atPath('identifierInNamespace')
-                            ->addViolation();
+                        $uniqueIdentifiant = false;
                         break;
                     }
                 }
+                //Il faut aussi boucler sur les identifiants des classes
+                foreach ($namespace->getClasses() as $class) {
+                    if ($class->getIdentifierInNamespace() == $this->identifierInNamespace) {
+                        $uniqueIdentifiant = false;
+                        break;
+                    }
+                }
+            }
+            if(!$uniqueIdentifiant){
+                $context->buildViolation('The identifier must be unique. Please enter another one.')
+                    ->atPath('identifierInNamespace')
+                    ->addViolation();
             }
         }
     }
