4f8aaf9d07c5835d76bd8417a2bdcd9dde9e5c90
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2023-12-14 11:57:31 +0100

    Les listes Subproperties et Superproperties sont triées, dans summary des propriétés

--- src/AppBundle/Entity/Property.php
+++ src/AppBundle/Entity/Property.php
@@ -559,9 +559,19 @@ class Property
         $this->targetEntityAssociations = $targetEntityAssociations;
     }
 
+    /**
+     * @return ArrayCollection|PropertyAssociation[]
+     */
     public function getEntityAssociations()
     {
-        return array_merge($this->getSourceEntityAssociations()->toArray(), $this->getTargetEntityAssociations()->toArray());
+        $entityAssociations = new ArrayCollection();
+        foreach ($this->sourceEntityAssociations as $entityAssociation){
+            $entityAssociations->add($entityAssociation);
+        }
+        foreach ($this->targetEntityAssociations as $entityAssociation){
+            $entityAssociations->add($entityAssociation);
+        }
+        return $entityAssociations;
     }
 
     /**
