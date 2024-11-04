35f10e71b655c2bb762e7fe9917c83d88d4a2f65
Alexandre Perraud <alexandre.perraud@ish-lyon.cnrs.fr>
2024-11-04 16:03:27 +0100

    Devalidation

--- src/AppBundle/Controller/EntityAssociationController.php
+++ src/AppBundle/Controller/EntityAssociationController.php
@@ -363,8 +363,8 @@ class EntityAssociationController extends Controller
         //Verifier que les références sont cohérents
         $nsRefsEntityAssociation = $entityAssociation->getNamespaceForVersion()->getAllReferencedNamespaces();
         $nsRefsEntityAssociation->add($entityAssociation->getNamespaceForVersion());
-        $nsSource = $entityAssociation->getParentClassNamespace();
-        $nsTarget = $entityAssociation->getChildClassNamespace();
+        $nsSource = $entityAssociation->getSourceNamespaceForVersion();
+        $nsTarget = $entityAssociation->getTargetNamespaceForVersion();
         if(!$nsRefsEntityAssociation->contains($nsSource) || !$nsRefsEntityAssociation->contains($nsTarget)){
             $uriNamespaceMismatches = $this->generateUrl('namespace_show', ['id' => $entityAssociation->getNamespaceForVersion()->getId(), '_fragment' => 'mismatches']);
             $this->addFlash('warning', 'This relation can\'t be validated. Check <a href="'.$uriNamespaceMismatches.'">mismatches</a>.');
